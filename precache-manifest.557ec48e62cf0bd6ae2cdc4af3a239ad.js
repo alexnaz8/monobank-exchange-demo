57a27687b5c3ae51265a472bfe88d430a68aa994
Alex Nazarenko <alexnaz8@gmail.com>
2020-01-30 21:00:15 +0200

    Updates

---
+++ precache-manifest.557ec48e62cf0bd6ae2cdc4af3a239ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce37a630523e6f33e60342ce556c293",
+    "url": "/github.io/monobank-exchange-demo/index.html"
+  },
+  {
+    "revision": "ecb0b97a6c0a763b47db",
+    "url": "/github.io/monobank-exchange-demo/static/css/main.7a413f47.chunk.css"
+  },
+  {
+    "revision": "dce2a2d5e63654f1b68e",
+    "url": "/github.io/monobank-exchange-demo/static/js/2.059316bf.chunk.js"
+  },
+  {
+    "revision": "ecb0b97a6c0a763b47db",
+    "url": "/github.io/monobank-exchange-demo/static/js/main.90e8fce1.chunk.js"
+  },
+  {
+    "revision": "4b37dc1c32a4bf150981",
+    "url": "/github.io/monobank-exchange-demo/static/js/runtime-main.29524624.js"
+  }
+]);
